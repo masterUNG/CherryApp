37960a33330310e56bd037a88b807241eb479c69
MasterUNG <phrombutr@gmail.com>
2018-02-17 15:31:42 +0700

    Design UI Main Finish

--- app/src/main/java/masterung/androidthai/in/th/cherryapp/MainActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/MainActivity.java
@@ -3,11 +3,24 @@ package masterung.androidthai.in.th.cherryapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.cherryapp.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    }   // Main Method
+
+
+}   // Main Class
