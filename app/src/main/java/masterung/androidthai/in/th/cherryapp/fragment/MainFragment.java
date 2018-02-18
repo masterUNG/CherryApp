c8628a01eb7aae75e5ea215d97368c782a7c1591
MasterUNG <phrombutr@gmail.com>
2018-02-18 14:53:24 +0700

    Authen Success

--- app/src/main/java/masterung/androidthai/in/th/cherryapp/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/fragment/MainFragment.java
@@ -3,12 +3,22 @@ package masterung.androidthai.in.th.cherryapp.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import masterung.androidthai.in.th.cherryapp.R;
+import masterung.androidthai.in.th.cherryapp.utility.GetAllDataFromServer;
+import masterung.androidthai.in.th.cherryapp.utility.MyAlert;
+import masterung.androidthai.in.th.cherryapp.utility.MyConstance;
 
 /**
  * Created by masterung on 17/2/2018 AD.
@@ -33,9 +43,93 @@ public class MainFragment extends Fragment{
                         .addToBackStack(null)
                         .commit();
 
-
             }
         });
+
+
+//        Login Controller
+        Button button = getView().findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText userEditText = getView().findViewById(R.id.editTextUser);
+                EditText passwordEditText = getView().findViewById(R.id.editTextPassword);
+
+                String userString = userEditText.getText().toString().trim();
+                String passwordString = passwordEditText.getText().toString().trim();
+
+                if (userString.isEmpty() || passwordString.isEmpty()) {
+
+//                    Have Space
+                    MyAlert myAlert = new MyAlert(getActivity());
+                    myAlert.dialogNormal("Have Space",
+                            "Please Fill User and Password");
+
+                } else {
+
+//                    No Space
+                    try {
+
+                        String tag = "18FebV1";
+                        MyConstance myConstance = new MyConstance();
+                        boolean statusBoolean = true;   // true ==> User False
+                        String resultJSON, nameString = null, passwordTrueString = null;
+                        MyAlert myAlert = new MyAlert(getActivity());
+
+                        GetAllDataFromServer getAllDataFromServer = new GetAllDataFromServer(getActivity());
+                        getAllDataFromServer.execute(myConstance.getUrlGetAllData());
+
+                        resultJSON = getAllDataFromServer.get();
+                        Log.d(tag, "JSON ==> " + resultJSON);
+
+                        JSONArray jsonArray = new JSONArray(resultJSON);
+                        for (int i=0; i<jsonArray.length(); i+=1) {
+
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString("User"))) {
+
+                                statusBoolean = false;
+                                nameString = jsonObject.getString("Name");
+                                passwordTrueString = jsonObject.getString("Password");
+
+                            }   // if
+
+                        }   // for
+
+                        if (statusBoolean) {
+                            myAlert.dialogNormal("User False",
+                                    "No " + userString + " in my Database");
+                        } else if (passwordString.equals(passwordTrueString)) {
+
+                            Toast.makeText(getActivity(), "Welcome " + nameString,
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            myAlert.dialogNormal("Password False",
+                                    "Please Try Again Password False");
+
+                        }
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+
+                }   // if
+
+
+
+
+            }   // onClick
+        });
+
+
+
 
     }   // Main Method
 
--- app/src/main/java/masterung/androidthai/in/th/cherryapp/utility/MyConstance.java
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/utility/MyConstance.java
@@ -7,6 +7,11 @@ package masterung.androidthai.in.th.cherryapp.utility;
 public class MyConstance {
 
     private String urlPostData = "http://androidthai.in.th/29oct/addDataMaster.php";
+    private String urlGetAllData = "http://androidthai.in.th/29oct/getAllDataMaster.php";
+
+    public String getUrlGetAllData() {
+        return urlGetAllData;
+    }
 
     public String getUrlPostData() {
         return urlPostData;
