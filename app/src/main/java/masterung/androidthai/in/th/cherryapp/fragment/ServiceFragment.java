8be7abf3d2e558de5188ebc4251cd3dd1ea86761
MasterUNG <phrombutr@gmail.com>
2018-02-18 15:54:43 +0700

    Project Success

--- app/src/main/java/masterung/androidthai/in/th/cherryapp/fragment/ServiceFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/fragment/ServiceFragment.java
@@ -7,9 +7,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import masterung.androidthai.in.th.cherryapp.MainActivity;
 import masterung.androidthai.in.th.cherryapp.R;
+import masterung.androidthai.in.th.cherryapp.utility.GetAllDataFromServer;
+import masterung.androidthai.in.th.cherryapp.utility.MyAlert;
+import masterung.androidthai.in.th.cherryapp.utility.MyConstance;
 
 /**
  * Created by masterung on 18/2/2018 AD.
@@ -38,7 +47,55 @@ public class ServiceFragment extends Fragment{
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(getArguments().getString("Name"));
 
 
-    }
+//        Create ListView
+        ListView listView = getView().findViewById(R.id.listViewName);
+        try {
+
+            MyConstance myConstance = new MyConstance();
+            final MyAlert myAlert = new MyAlert(getActivity());
+            GetAllDataFromServer getAllDataFromServer = new GetAllDataFromServer(getActivity());
+            getAllDataFromServer.execute(myConstance.getUrlGetAllData());
+
+            JSONArray jsonArray = new JSONArray(getAllDataFromServer.get());
+
+            final String[] nameStrings = new String[jsonArray.length()];
+            final String[] userStrings = new String[jsonArray.length()];
+            final String[] passwordStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length(); i+=1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("Name");
+                userStrings[i] = jsonObject.getString("User");
+                passwordStrings[i] = jsonObject.getString("Password");
+
+            }   //for
+
+            ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(
+                    getActivity(),
+                    android.R.layout.simple_list_item_1,
+                    nameStrings
+            );
+            listView.setAdapter(stringArrayAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+
+                    myAlert.dialogNormal(nameStrings[i],
+                            "User ==> " + userStrings[i] +
+                                    "\n" + "Password ==> " + passwordStrings[i]);
+
+                }
+            });
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }   // Main Method
 
     @Nullable
     @Override
