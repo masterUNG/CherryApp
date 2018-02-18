f752daa725c4d7f7ee43e6dc2be8db95c66fe782
MasterUNG <phrombutr@gmail.com>
2018-02-18 11:54:04 +0700

    before Lunch

---
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/utility/MyConstance.java
@@ -0,0 +1,14 @@
+package masterung.androidthai.in.th.cherryapp.utility;
+
+/**
+ * Created by masterung on 18/2/2018 AD.
+ */
+
+public class MyConstance {
+
+    private String urlPostData = "http://androidthai.in.th/29oct/addDataMaster.php";
+
+    public String getUrlPostData() {
+        return urlPostData;
+    }
+}
---
+++ app/src/main/java/masterung/androidthai/in/th/cherryapp/utility/PostDataToServer.java
@@ -0,0 +1,16 @@
+package masterung.androidthai.in.th.cherryapp.utility;
+
+import android.content.Context;
+
+/**
+ * Created by masterung on 18/2/2018 AD.
+ */
+
+public class PostDataToServer {
+
+    private Context context;
+
+    public PostDataToServer(Context context) {
+        this.context = context;
+    }
+}   // Main Class
